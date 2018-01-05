bcaa68462ee5ee1d06018403d970df22e938eff1
Edgar Silva <edgar.silva@gmail.com>
2018-01-05 20:42:19 -0200

    adding doc

--- enterprise-integrator/file-systems/README.md
+++ enterprise-integrator/file-systems/README.md
@@ -1,1 +1,7 @@
 # Samples : File Systems
+
+Here is the documentation in how to checkout, mount and execute this sample/demo
+
+https://docs.google.com/document/d/1BhCxivn8Y53EwI7bZCoPspOa7TgqOSkVZxQ-S8KEPBc/edit?usp=sharing
+
+Author: Edgar Silva @jedgarsilva 
---
+++ enterprise-integrator/file-systems/runSample.sh
@@ -0,0 +1,10 @@
+clear
+echo Running WSO2 Enterprise Integrator Demo on File Systems
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+cp rentacar/files/cars-sao-paulo-airpor-CGH.xml rentacar/files/in/
+echo done!
